b702e3c19816a2b07ff87631f8f3f791ebffc6b4
gkappaga <gkappaga@caltech.edu>
2025-05-15 23:21:20 -0700

    Modifying SLURM Part 3

--- slurm_scripts/slurm_train.sh
+++ slurm_scripts/slurm_train.sh
@@ -21,5 +21,5 @@
 # module load cuda/12.2  # Adjusted to CUDA version 12.2
 
 # Change to the directory containing v2_run_fine_tuning.sh
-cd $HOME/gkappaga-DAlearning/scripts
+cd $HOME/gkappaga-DALearning/scripts
 bash run_train.sh
