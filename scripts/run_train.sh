99ff1f938e82f9dd89db104033a3d28524291b8e
gkappaga <gkappaga@caltech.edu>
2025-05-23 13:38:10 -0700

    modifying slurm script to have both learnk and affine

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -22,6 +22,14 @@ python train.py \
     --v 'LearnK' \
     --epochs 50
 
+python train.py \
+    --dataset lorenz96 \
+    --N 10 \
+    --sigma_y 1 \
+    --seed 42 \
+    --v 'Affine' \
+    --epochs 50
+
 # python train.py \
 #     --dataset lorenz96 \
 #     --N 10 \
