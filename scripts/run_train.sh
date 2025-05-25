9fe92fa137b6235e4ad512949462120aa1416ef1
gkappaga <gkappaga@caltech.edu>
2025-05-25 16:21:05 -0700

    Saving every epoch

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -20,7 +20,8 @@ python train.py \
     --sigma_y 1 \
     --seed 42 \
     --v 'LearnK' \
-    --epochs 50
+    --epochs 50 \
+    --save_epoch 1
 
 python train.py \
     --dataset lorenz96 \
@@ -28,7 +29,8 @@ python train.py \
     --sigma_y 1 \
     --seed 42 \
     --v 'Affine' \
-    --epochs 50
+    --epochs 50 \
+    --save_epoch 1
 
 # python train.py \
 #     --dataset lorenz96 \
