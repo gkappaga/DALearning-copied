965493e757df82ed55c531ac084897bd068dfd2d
gkappaga <gkappaga@caltech.edu>
2025-06-03 13:59:02 -0700

    Fixing Cvy shape

--- training_utils.py
+++ training_utils.py
@@ -213,7 +213,7 @@ def train_model(epoch, loader, model_list, optimizer, scheduler, args, H_info=No
 
                 Cvv = torch.bmm(Vp, Vp.transpose(1, 2)) / (N - 1)
                 Cyy = torch.bmm(Hp, Hp.transpose(1, 2)) / (N - 1) #+ args.sigma_y**2 * torch.eye(d, device=args.device).unsqueeze(0)
-                Cvy = torch.bmm(Vp, Hp.transpose(1, 2)) / (N - 1)
+                Cvy = torch.bmm(Vp.transpose(1, 2), Hp) / (N - 1)
                 Cyv = Cvy.transpose(1, 2)
 
                 Cyy_inv = torch.inverse(Cyy)
