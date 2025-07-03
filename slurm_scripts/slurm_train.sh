35df45c5b59d6cdd8e51554b196affe80348d6ab
gkappaga <gkappaga@caltech.edu>
2025-07-03 13:30:43 -0700

    7/3

--- slurm_scripts/slurm_train.sh
+++ slurm_scripts/slurm_train.sh
@@ -2,9 +2,9 @@
 
 # Submit this script with: sbatch <this-filename>
 
-#SBATCH --time=2-00:00:00     # walltime (2 days)
+#SBATCH --time=02:00:00     # walltime (2 days)
 #SBATCH --nodes=1           # number of nodes (1 node)
-#SBATCH --gres=gpu:4        # 4 GPUs of any type
+#SBATCH --gres=gpu:1        # 4 GPUs of any type
 #SBATCH --partition=gpu     # use GPU partition
 #SBATCH --ntasks=1          # 1 task
 #SBATCH -J "gautham-gpu-LearnAffine"   # job name
