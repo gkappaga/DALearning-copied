ce27f62be2fadda5b9a05537698c062165ca0881
gkappaga <gkappaga@caltech.edu>
2025-07-07 09:29:16 -0700

    7/7

--- training_utils.py
+++ training_utils.py
@@ -373,10 +373,10 @@ def train_model(epoch, loader, model_list, optimizer, scheduler, args, H_info=No
                 #     if p.grad is not None:
                 #         print(name, "grad norm:", p.grad.norm().item())
 
-                
+                nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer.zero_grad()
                 loss.backward()
-                nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                
                 optimizer.step()
         
         total_count += batch_v.shape[1]
