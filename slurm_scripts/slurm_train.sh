331e803bc41f640f84d979bc69a97a9ecfcb9edb
gkappaga <gkappaga@caltech.edu>
2025-07-08 08:53:26 -0700

    7/8

--- slurm_scripts/slurm_train.sh
+++ slurm_scripts/slurm_train.sh
@@ -2,7 +2,7 @@
 
 # Submit this script with: sbatch <this-filename>
 
-#SBATCH --time=02:00:00     # walltime (2 days)
+#SBATCH --time=12:00:00     # walltime (2 days)
 #SBATCH --nodes=1           # number of nodes (1 node)
 #SBATCH --gres=gpu:1        # 4 GPUs of any type
 #SBATCH --partition=gpu     # use GPU partition
