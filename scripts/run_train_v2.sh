6c4949ed7e5cefeb91daed6db9ccd838ab0867b7
gkappaga <gkappaga@caltech.edu>
2025-07-08 10:59:48 -0700

    7/8

--- scripts/run_train_v2.sh
+++ scripts/run_train_v2.sh
@@ -4,39 +4,52 @@ cd ..
 
 python train.py \
     --dataset lorenz96 \
-    --learning_rate 1e-4 \
     --N 10 \
     --sigma_y 1 \
     --seed 42 \
-    --loss_type es \
-    --lr_decay_epochs 400,800,1200,1600,2000,2400 \
-    --lr_decay_rate 0.7 \
-    --epochs 3000 \
-    --v EtE \
-    --warm_up
+    --v 'Affine' \
+    --epochs 1000 \
+    --save_epoch 50 \
+    --loss_type 'nes' \
+    --mc_penalty True \
+    --lambda1 0 \
+    --lambda2 0.0001
 
-python train.py \
-    --dataset lorenz96 \
-    --learning_rate 1e-4 \
-    --N 10 \
-    --sigma_y 1 \
-    --seed 42 \
-    --loss_type nes \
-    --lr_decay_epochs 400,800,1200,1600,2000,2400 \
-    --lr_decay_rate 0.7 \
-    --epochs 3000 \
-    --v EtE \
-    --warm_up
+# python train.py \
+#     --dataset lorenz96 \
+#     --learning_rate 1e-4 \
+#     --N 10 \
+#     --sigma_y 1 \
+#     --seed 42 \
+#     --loss_type es \
+#     --lr_decay_epochs 400,800,1200,1600,2000,2400 \
+#     --lr_decay_rate 0.7 \
+#     --epochs 3000 \
+#     --v EtE \
+#     --warm_up
 
-python train.py \
-    --dataset lorenz96 \
-    --learning_rate 1e-2 \
-    --N 10 \
-    --sigma_y 1 \
-    --seed 42 \
-    --loss_type tnes \
-    --lr_decay_epochs 400,800,1200,1600,2000,2400 \
-    --lr_decay_rate 0.7 \
-    --epochs 3000 \
-    --v EtE \
-    --warm_up
+# python train.py \
+#     --dataset lorenz96 \
+#     --learning_rate 1e-4 \
+#     --N 10 \
+#     --sigma_y 1 \
+#     --seed 42 \
+#     --loss_type nes \
+#     --lr_decay_epochs 400,800,1200,1600,2000,2400 \
+#     --lr_decay_rate 0.7 \
+#     --epochs 3000 \
+#     --v EtE \
+#     --warm_up
+
+# python train.py \
+#     --dataset lorenz96 \
+#     --learning_rate 1e-2 \
+#     --N 10 \
+#     --sigma_y 1 \
+#     --seed 42 \
+#     --loss_type tnes \
+#     --lr_decay_epochs 400,800,1200,1600,2000,2400 \
+#     --lr_decay_rate 0.7 \
+#     --epochs 3000 \
+#     --v EtE \
+#     --warm_up
