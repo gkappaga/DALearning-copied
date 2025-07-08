bf7670facbb10251eb02d0c806e1d3d7d3a7bf75
gkappaga <gkappaga@caltech.edu>
2025-07-07 21:06:39 -0700

    7/7

--- config/cli.py
+++ config/cli.py
@@ -196,6 +196,7 @@ def get_parameters():
     parser.add_argument('--GPU_memory', type=int, default=16, help='GPU memory in GB')
     parser.add_argument('--normal_output', action='store_true', help='Just print the output without redirecting to a .txt file')
     parser.add_argument('--suffix', type=str, default="", help='save folder suffix')
+    parser.add_argument('--test_only', action='store_true', help='Only do the test part')
 
     # version setting
     parser.add_argument('--v', type=str, choices=['CorrTerms','EtE','EtE-LRes','EtE2','EnKF','ESRF','LETKF', 'LearnK', 'Affine'],
