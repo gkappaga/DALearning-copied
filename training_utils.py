ceffff0fbfa842ff6047bc912140f2076c5cf941
gkappaga <gkappaga@caltech.edu>
2025-07-09 21:34:12 -0700

    7/9

--- training_utils.py
+++ training_utils.py
@@ -250,7 +250,7 @@ def train_model(epoch, loader, model_list, optimizer, scheduler, args, H_info=No
                 # multiply aydag with the true observation
                 Aydag_y = torch.bmm(A_ydag, obs_y.permute(0, 2, 1))
                 Aydag_y = Aydag_y.permute(0, 2, 1)
-                ens_v_a = Av + Ayhat + Aydag_y + ens_v_f
+                ens_v_a = Av + Ayhat - Aydag_y
 
 
             ens_v_a = torch.clamp(ens_v_a, min=-args.clamp, max=args.clamp)
