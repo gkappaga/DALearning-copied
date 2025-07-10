501ecd26f96bfecb80fbb4e3a551d0e680856e2f
gkappaga <gkappaga@caltech.edu>
2025-07-09 21:06:46 -0700

    7/9

--- config/cli.py
+++ config/cli.py
@@ -200,7 +200,7 @@ def get_parameters():
     parser.add_argument('--redirect_output', action='store_true', help='Redirect the output to a txt file')
 
     # version setting
-    parser.add_argument('--v', type=str, choices=['CorrTerms','EtE','EtE-LRes','EtE2','EnKF','ESRF','LETKF', 'LearnK', 'Affine'],
+    parser.add_argument('--v', type=str, choices=['CorrTerms','EtE','EtE-LRes','EtE2','EnKF','ESRF','LETKF', 'LearnK', 'Affine', 'Affine-ydagger'],
                         default='LETKF', help='versions')
 
     args = parser.parse_args()
