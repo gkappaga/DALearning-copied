15134fabc6464e3a3177a7cb74fa328ce7b2dcc2
gkappaga <gkappaga@caltech.edu>
2025-07-11 22:27:19 -0700

    7/11

--- training_utils.py
+++ training_utils.py
@@ -879,19 +879,19 @@ def set_models(args):
             d_input  = args.input_dim,
             d_output = args.ori_dim**2,
             num_hidden_layers=3,
-            latent_dim = 32
+            latent_dim = 64
         ).to(args.device)
         model_Ayhat = Simple_MLP(
             d_input  = args.input_dim,
             d_output = args.ori_dim * args.obs_dim,
             num_hidden_layers=3,
-            latent_dim = 32 
+            latent_dim = 64 
         ).to(args.device)
         model_Aydag = Simple_MLP(
             d_input  = args.input_dim,
             d_output = args.ori_dim * args.obs_dim,
             num_hidden_layers=3,
-            latent_dim = 32
+            latent_dim = 64
         ).to(args.device)
         infl_model  = NaiveNetwork(1)
         local_model = NaiveNetwork(1)
