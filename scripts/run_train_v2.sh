c2c448bf5403eb3caae40337d84563902a880a89
gkappaga <gkappaga@caltech.edu>
2025-07-20 20:50:33 -0700

    7/20

--- scripts/run_train_v2.sh
+++ scripts/run_train_v2.sh
@@ -11,6 +11,8 @@ python train.py \
     --epochs 1000 \
     --save_epoch 50 \
     --loss_type 'nl2' \
+    --lr_decay_epochs 200,300,400,500,600,700,800,900 \
+    --lr_decay_rate 0.7 \
 # python train.py \
 #     --dataset lorenz96 \
 #     --learning_rate 1e-4 \
