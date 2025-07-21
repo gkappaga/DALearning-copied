51940f43683f62c365669d824afaa4c78045d34e
gkappaga <gkappaga@caltech.edu>
2025-07-21 11:55:43 -0700

    7/21

--- scripts/run_train_v2.sh
+++ scripts/run_train_v2.sh
@@ -11,20 +11,16 @@ python train.py \
     --epochs 1000 \
     --save_epoch 50 \
     --loss_type 'nl2' \
-    --lr_decay_epochs 200,300,400,500,600,700,800,900 \
-    --lr_decay_rate 0.7 \
-# python train.py \
-#     --dataset lorenz96 \
-#     --learning_rate 1e-4 \
-#     --N 10 \
-#     --sigma_y 1 \
-#     --seed 42 \
-#     --loss_type es \
-#     --lr_decay_epochs 400,800,1200,1600,2000,2400 \
-#     --lr_decay_rate 0.7 \
-#     --epochs 3000 \
-#     --v EtE \
-#     --warm_up
+
+python train.py \
+    --dataset lorenz96 \
+    --N 10 \
+    --sigma_y 1 \
+    --seed 42 \
+    --v 'Affine-ydagger' \
+    --epochs 1000 \
+    --save_epoch 50 \
+    --loss_type 'nl2' \
 
 # python train.py \
 #     --dataset lorenz96 \
